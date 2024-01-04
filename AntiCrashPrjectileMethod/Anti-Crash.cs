0f3617a937226631ce904b2ce6d3b90504930f07
gilbe <gilbe@DMG-DELL-2022>
2024-01-03 21:51:36 -0600

    Add project files.

---
+++ AntiCrashPrjectileMethod/Anti-Crash.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using GorillaLocomotion;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+
+namespace Plugin
+{
+    [HarmonyPatch(typeof(GorillaGameManager), "LaunchSlingshotProjectile", MethodType.Normal)]
+
+    public class AntiCrashPatch
+    {
+        private static bool Prefix(Vector3 slingshotLaunchLocation, Vector3 slingshotLaunchVelocity, int projHash, int trailHash, bool forLeftHand, int projectileCount, bool shouldOverrideColor, float colorR, float colorG, float colorB, float colorA, PhotonMessageInfo info)
+        {
+            bool flag = info.Sender != PhotonNetwork.LocalPlayer;
+            if (flag)
+            {
+                bool flag2 = Vector3.Distance(GorillaGameManager.instance.FindPlayerVRRig(info.Sender).transform.position, slingshotLaunchLocation) > 1.5f;
+                if (flag2)
+                {
+                    return false;
+                }
+                bool flag3 = Vector3.Distance(slingshotLaunchLocation, Player.Instance.transform.position) > 10f;
+                if (flag3)
+                {
+                    return false;
+                }
+                bool flag4 = ObjectPools.instance.GetPoolByHash(projHash).objectToPool.GetComponent<SlingshotProjectileTrail>() != null;
+                if (flag4)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
---
+++ AntiCrashPrjectileMethod/Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Plugin
+{
+    [BepInPlugin("com.Bobo.Anti-Crash", "Anti-Crash", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void OnEnable()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+        }
+
+        private void OnDisable()
+        {
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        private void OnGUI()
+        {
+            GUI.backgroundColor = Color.clear;
+            Main.Toggle = GUI.Toggle(new Rect(-10f, 1070f, 200f, 200f), Main.Toggle, " ");
+            if (Main.Toggle)
+            {
+                GUI.skin.label.fontSize = 15;
+                GUI.skin.label.fontStyle = FontStyle.BoldAndItalic;
+                GUI.color = Color.magenta;
+                GUI.backgroundColor = Color.black;
+                this.modcheck = GUILayout.Toggle(this.modcheck, "ModCheck", Array.Empty<GUILayoutOption>());
+                if (this.modcheck && PhotonNetwork.InRoom)
+                {
+                    GUILayout.BeginVertical(Array.Empty<GUILayoutOption>());
+                    foreach (Player player in PhotonNetwork.PlayerList)
+                    {
+                        string[] array = new string[7];
+                        array[0] = "<b>( USERID : <color=grey>";
+                        array[1] = player.UserId;
+                        array[2] = "</color>, NAME : <color=grey>";
+                        array[3] = player.NickName;
+                        array[4] = "</color>, UTILLA MODS GUIDS : <color=red>";
+                        int num = 5;
+                        object obj = player.CustomProperties["mods"];
+                        array[num] = ((obj != null) ? obj.ToString() : null);
+                        array[6] = "</color>)</b>\n";
+                        GUILayout.Label(string.Concat(array), Array.Empty<GUILayoutOption>());
+                    }
+                    GUILayout.EndHorizontal();
+                }
+                GUILayout.Space(10f);
+                if (PhotonNetwork.InRoom)
+                {
+                    GUILayout.Label("In Room", Array.Empty<GUILayoutOption>());
+                }
+            }
+        }
+
+        public static bool Toggle = true;
+
+        private bool modcheck;
+    }
+}
